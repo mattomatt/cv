ac66f9ef9acd2b361d33206b6a39d18111a2f8b2
Matteo Tassetti <mtassetti@anastasis.it>
2014-12-28 11:35:36 +0100

    aggiunto linkedin

--- www/js/layout/layoutControllers.js
+++ www/js/layout/layoutControllers.js
@@ -4,6 +4,21 @@
 
 angular.module('layout')
 
-    .controller('AppCtrl',['$scope', function($scope) {
+    .controller('AppCtrl', ['$scope', function ($scope) {
         console.log('AppCtrl');
+        $scope.downloadPdf=function(){
+
+            window.plugins.webintent.startActivity({
+                    action: window.plugins.webintent.ACTION_VIEW,
+                    type:'application/pdf',
+                    url: 'file:///assets/www/res/curriculum.pdf'
+                },
+                function(){
+                    console.log('chiamata ok')
+                },
+                function(e){
+                    console.log('errore chiamata')
+                }
+            );
+        };
     }]);
